775448d38723ce5e8edb2aa792be82fca3934303
codingmaven <codingmaven@gmail.com>
2016-09-01 18:22:20 +0300

    more logging

--- api/common/log.js
+++ api/common/log.js
@@ -1,22 +1,23 @@
+/**
+ * Created by DreamSoft on 7/30/2015.
+ */
 'use strict';
 
 var through = require('through');
 var winston = require('winston');
-require('winston-papertrail').Papertrail;
 
 var logger = new winston.Logger({
   transports: [
-    new (winston.transports.Papertrail)({
+    new (winston.transports.Console)({
       silent: process.env.NODE_ENV === 'test',
-      host: 'logs4.papertrailapp.com',
-      port: 50493
-    }),
-
+      colorize: true,
+      timestamp: true
+    })
   ]
 });
 
-logger.asStream = function asStream(level){
-  return through(function(data){
+logger.asStream = function asStream(level) {
+  return through(function (data) {
     logger.log(level, String(data));
   });
 };
