28f703a622fa0793e365647f842d1c2c193cf2e1
Larby Amirouche <larby@tumedia.net>
2016-10-11 17:45:27 -0400

    Update log.js

--- api/common/log.js
+++ api/common/log.js
@@ -2,21 +2,19 @@
 
 var through = require('through');
 var winston = require('winston');
-require('winston-papertrail').Papertrail;
 
 var logger = new winston.Logger({
   transports: [
-    new (winston.transports.Papertrail)({
+    new (winston.transports.Console)({
       silent: process.env.NODE_ENV === 'test',
-      host: 'logs4.papertrailapp.com',
-      port: 50493
-    }),
-
+      colorize: true,
+      timestamp: true
+    })
   ]
 });
 
-logger.asStream = function asStream(level){
-  return through(function(data){
+logger.asStream = function asStream(level) {
+  return through(function (data) {
     logger.log(level, String(data));
   });
 };
