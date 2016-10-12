216541852ff15d4fba64723eac21a58973470173
Larby Amirouche <larby@tumedia.net>
2016-10-12 16:13:14 -0400

    Revert "Update log.js"
    This reverts commit 28f703a622fa0793e365647f842d1c2c193cf2e1.

--- api/common/log.js
+++ api/common/log.js
@@ -2,19 +2,21 @@
 
 var through = require('through');
 var winston = require('winston');
+require('winston-papertrail').Papertrail;
 
 var logger = new winston.Logger({
   transports: [
-    new (winston.transports.Console)({
+    new (winston.transports.Papertrail)({
       silent: process.env.NODE_ENV === 'test',
-      colorize: true,
-      timestamp: true
-    })
+      host: 'logs4.papertrailapp.com',
+      port: 50493
+    }),
+
   ]
 });
 
-logger.asStream = function asStream(level) {
-  return through(function (data) {
+logger.asStream = function asStream(level){
+  return through(function(data){
     logger.log(level, String(data));
   });
 };
