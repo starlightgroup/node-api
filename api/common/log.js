89c97ddd00be79b8ef2750a9c2fbf0eead220e39
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2016-10-13 23:32:24 -0400

    Logger write to File and Console + PaperTrail Removed

--- api/common/log.js
+++ api/common/log.js
@@ -2,16 +2,11 @@
 
 var through = require('through');
 var winston = require('winston');
-require('winston-papertrail').Papertrail;
 
 var logger = new winston.Logger({
   transports: [
-    new (winston.transports.Papertrail)({
-      silent: process.env.NODE_ENV === 'test',
-      host: 'logs4.papertrailapp.com',
-      port: 50493
-    }),
-
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({ filename: 'server-log.log' })
   ]
 });
 
