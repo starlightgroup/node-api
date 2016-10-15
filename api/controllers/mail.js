83f725d189249787621907e0736dc52c1a5451fd
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2016-10-15 00:18:17 -0400

    leadoutpostCtrl

--- api/controllers/mail.js
+++ api/controllers/mail.js
@@ -10,90 +10,8 @@ import phone from 'phone';
 const autopilot = new Autopilot(config.autopilot.key);
 
 
-async function migrate(req, res, next) {
-    let contacts = await autopilot.lists.roster(config.autopilot.clientlist, 'person_0E8607F2-E308-438F-BF16-FB627DB4A4C9');
-    while(contacts.data.contacts.length >= 100) {
-        let contact = {};
-        for(contact of contacts.data.contacts) {
-            if(contact.Phone && phone(contact.Phone, 'US')[0]) {
-                const leadoutpost = {
-                    firstName: contact.FirstName,
-                    lastName: contact.LastName,
-                    email: contact.Email,
-                    phone: contact.Phone
-                };
-                leadoutpost.apiKey = config.leadoutpost.apiKey;
-                leadoutpost.campaignId = config.leadoutpost.campaignId;
-                const options = {
-                    uri: 'https://www.leadoutpost.com/api/v1/lead',
-                    qs: leadoutpost,
-                    headers: {
-                        'User-Agent': 'Request-Promise'
-                    },
-                    json: true // Automatically parses the JSON string in the response
-                };
-                await request.post(options);
-                console.log(contact.contact_id, contact.Email, contact.Phone, contact.FirstName, contact.LastName);
-            }
-        }
-        contacts = await autopilot.lists.roster(config.autopilot.clientlist, contact.contact_id);
-        console.log("last----------------", contact.contact_id);
-    }
-    res.success({length: contacts.data.contacts.length});
 
-}
-/*
- * add contact to autopilot
- *
- * req.body.Email
- * req.body.FirstName
- * req.body.LastName
- * req.body.Phone
- * req.body.MobilePhone
- * req.body.SkypeId
- * req.body.Browser
- *
- */
 
-async function addContact(req, res, next) {
-    try {
-        const leadoutpost = {
-            firstName: req.body.FirstName,
-            lastName: req.body.LastName,
-            email: req.body.Email,
-            phone: req.body.MobilePhone || req.body.Phone
-        };
-        if(!req.body.MobilePhone) {
-            req.body.MobilePhone = req.body.Phone;
-        }
-
-        if(!req.body.Phone) {
-            req.body.Phone = req.body.MobilePhone;
-        }
-        //await sendAffiliateEmail(req.body);
-        req.body._autopilot_list = config.autopilot.clientlist;
-        autopilot.contacts.upsert(req.body);
-
-        leadoutpost.apiKey = config.leadoutpost.apiKey;
-        leadoutpost.campaignId = config.leadoutpost.campaignId;
-
-        const options = {
-            uri: 'https://www.leadoutpost.com/api/v1/lead',
-            qs: leadoutpost,
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-
-        request.post(options);
-
-        res.success();
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-}
 
 
 async function addKonnektiveOrder(req, res, next) {
@@ -197,34 +115,7 @@ async function createKonnektiveLead(req, res, next) {
     }
 }
 
-async function updateContact(req, res, next) {
-    const contactData = mapToAutopilotJson(req.body);
-    const leadoutpostData = mapToLeadoutpostJson(req.body);
 
-    try {
-        //await sendAffiliateEmail(req.body);
-        contactData._autopilot_list = config.autopilot.clientlist;
-        autopilot.contacts.upsert(contactData);
-        res.success();
-
-        leadoutpostData.apiKey = config.leadoutpost.apiKey;
-        leadoutpostData.campaignId = config.leadoutpost.campaignId;
-
-        const options = {
-            uri: 'https://www.leadoutpost.com/api/v1/lead',
-            qs: leadoutpostData,
-            headers: {
-                'User-Agent': 'Request-Promise'
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-
-        request.post(options);
-    }
-    catch(error) {
-        return res.error(error.message);
-    }
-}
 
 async function upsell(req, res, next) {
     const {productId, productQty, orderId} = req.body;
@@ -278,23 +169,6 @@ async function getIpinfo(req, res, next) {
     //i am hardcoding our token in here because i don't give a fuck
     ipinfo = JSON.parse(await request(`https://ipinfo.io/${clientIp}/json/?token=1f4c1ea49e0aa2`));
     res.send(ipinfo);
-}
-
-async function addLeadoutpost(req, res, next) {
-    req.body.apiKey = config.leadoutpost.apiKey;
-    req.body.campaignId = config.leadoutpost.campaignId;
-
-    const options = {
-        uri: 'https://www.leadoutpost.com/api/v1/lead',
-        qs: req.body,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
-
-    const response = await request.post(options);
-    res.send(response);
 }
 
 function mapToStateDetails(data) {
@@ -359,20 +233,16 @@ async function ping(req,res, next) {
 }
 
 export default {
-    addContact: addContact,
     getLead: getLead,
     createKonnektiveLead: createKonnektiveLead,
     //sendSMS: sendSMS,
-    updateContact: updateContact,
     //sendSMS2: sendSMS2,
     upsell: upsell,
     getStateInfo: getStateInfo,
     triggerJourney: triggerJourney,
     getTrans: getTrans,
     getIpinfo: getIpinfo,
-    addLeadoutpost: addLeadoutpost,
     addKonnektiveOrder: addKonnektiveOrder,
     verifyPhoneNumber: verifyPhoneNumber,
-    ping : ping,
-    migrate: migrate,
+    ping : ping
 };
--- config/routes/v1.0.js
+++ config/routes/v1.0.js
@@ -1,5 +1,6 @@
 import mailCtrl from '../../api/controllers/mail';
 import smsCtrl from '../../api/controllers/sms';
+import leadoutpostCtrl from '../../api/controllers/leadoutpost';
 import resError from '../../api/middlewares/res_error';
 import resSuccess from '../../api/middlewares/res_success';
 
@@ -14,21 +15,23 @@ function route(router) {
   router.get('/get-trans/:id', mailCtrl.getTrans);
 
   router.get('/verify-phone/:phone', mailCtrl.verifyPhoneNumber);
-
-  router.post('/add-contact', mailCtrl.addContact);
+  
   router.post('/text/:contactId', smsCtrl.sendSMS);
   router.get('/text/:contactId', smsCtrl.sendSMS);
   router.get('/text2', smsCtrl.sendSMS2);
   router.post('/text2', smsCtrl.sendSMS2);
   router.get('/aphq', mailCtrl.triggerJourney);
   router.post('/aphq', mailCtrl.triggerJourney);
-  router.post('/update-contact', mailCtrl.updateContact);
+  
   router.get('/state/:stateNumber', mailCtrl.getStateInfo);
   router.get('/ipinfo', mailCtrl.getIpinfo);
   router.get('/ping' , mailCtrl.ping);
-  router.post('/add-leadoutpost', mailCtrl.addLeadoutpost);
+  
+  router.post('/add-contact', leadoutpostCtrl.addContact);
+  router.post('/update-contact', leadoutpostCtrl.updateContact);
+  router.post('/add-leadoutpost', leadoutpostCtrl.addLeadoutpost);
 
-  //router.get('/run-migrator', mailCtrl.migrate);
+  //router.get('/run-migrator', leadoutpostCtrl.migrate);
 };
 
 var routes = {v1_0 : route};
