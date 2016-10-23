1c50bc74bf0577e57f914b51db5848e45b810258
techgaun <coolsamar207@gmail.com>
2016-10-23 01:46:52 -0500

    rename scripts, exclude .git and make codedeploy faster

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Install dependencies"
+
+export API_DIR="/opt/node-api"
 if [[ "$DEPLOYMENT_GROUP_NAME" == "node-app1" ]]; then
   export NODE_ENV="production"
 else
@@ -7,3 +9,8 @@ else
 fi
 
 npm i -g babel
+
+if [[ -d "${API_DIR}" ]]; then
+  rm -rf "${API_DIR}"
+fi
+mkdir -vp "${API_DIR}"
