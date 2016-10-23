6b1541bef4f3f5bf25395cb83afc477ea01f450b
techgaun <coolsamar207@gmail.com>
2016-10-23 11:24:20 -0500

    don't remove node_modules

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo "Install dependencies"
-rm -rf /opt/node-api/node_modules
-cd /opt/node-api
-npm i
 
 echo "Change permission to ubuntu:ubuntu"
 chown -R ubuntu:ubuntu /opt/node-api
