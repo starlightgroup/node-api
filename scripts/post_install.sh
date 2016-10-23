9b5954d8e2a34fe870b5af1b4cc774b3d44faf88
techgaun <coolsamar207@gmail.com>
2016-10-23 01:54:12 -0500

    depend on travis dist creation as this does not get affected by difference in environments

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -5,10 +5,5 @@ rm -rf /opt/node-api/node_modules
 cd /opt/node-api
 npm i
 
-rm -rf dist
-babel . --ignore node_modules --out-dir dist --source-maps
-
-cp ./zip_code_database.csv ./dist/zip_code_database.csv
-
 echo "Change permission to ubuntu:ubuntu"
 chown -R ubuntu:ubuntu /opt/node-api
