75a7fd8fcc9b78dcf32f2f2e33eb03d2a479b3ae
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2016-10-27 03:06:24 -0400

    AP Key

--- config.default.js
+++ config.default.js
@@ -3,7 +3,8 @@
 module.exports = {
   sendgridkey: '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',
   autopilot: {
-    key: 'f2eeea98acd847dabcc1b430870f2cb5',
+    // key: 'f2eeea98acd847dabcc1b430870f2cb5',
+    key : '0a72c1c3812e4620a1500f3a256e1436',
     clientlist: 'contactlist_59EA0BF8-46D0-4733-B6C5-4F2EB7C890AA'
   },
   konnective: {
