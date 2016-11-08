095cf4af80769e7882af219f9ae9e6e983b6d1b4
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2016-11-08 16:52:47 -0500

    Redis

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Start NPM"
 if [[ "$DEPLOYMENT_GROUP_NAME" == "node-app1" ]]; then
-  export REDIS_URL="redis://prod-redis-01.rcanzp.ng.0001.use1.cache.amazonaws.com:6379"
+  export REDIS_URL="redis://localhost:6379"
   export NODE_ENV="production"
 else
   export NODE_ENV="staging"
