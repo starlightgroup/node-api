96935d81529975a36b93becca81c5daee9fe9b02
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-02-07 20:57:41 +0300

    more unit tests

--- test/api.spec.js
+++ test/api.spec.js
@@ -32,7 +32,7 @@ function extractCookie(res, rgx) {
 
 
 describe('web application', function () {
-  var
+  let
     sessionId, //being used in all requests
     csrfToken;
 
@@ -43,21 +43,20 @@ describe('web application', function () {
       .expect(200, {msg: 'PONG'})
       .end(function (error, res) {
         if (error) {
-          done(error);
-        } else {
-          // console.log('/api/v2/ping cookies ',res.headers['set-cookie']);
-          let sId = extractCookie(res, sessionIdCookieRegex);
-          if (sId === false) {
-            return done(new Error('PHPSESSID not set!'));
-          }
-          let csrf = extractCookie(res, csrfTokenCookieRegex);
-          if (csrf === false) {
-            return done(new Error('XSRF-TOKEN not set!'));
-          }
-          sessionId = sId;
-          csrfToken = csrf;
-          done();
+          return done(error);
         }
+        // console.log('/api/v2/ping cookies ',res.headers['set-cookie']);
+        let sId = extractCookie(res, sessionIdCookieRegex);
+        if (sId === false) {
+          return done(new Error('PHPSESSID not set!'));
+        }
+        let csrf = extractCookie(res, csrfTokenCookieRegex);
+        if (csrf === false) {
+          return done(new Error('XSRF-TOKEN not set!'));
+        }
+        sessionId = sId;
+        csrfToken = csrf;
+        done();
       });
   });
 
@@ -71,29 +70,28 @@ describe('web application', function () {
         .expect(200)
         .end(function (error, res) {
           if (error) {
-            done(error);
-          } else {
-            // console.log('/api/v2/testSession with session token cookies ',res.headers['set-cookie']);
-
-            res.body.ip.should.exist;
-            res.body.entryPoint.should.be.equal('/api/v2/ping');
-            res.body.userAgent.should.match(/^node-superagent/);
-
-            let csrf = extractCookie(res, csrfTokenCookieRegex);
-            if (csrf === false) {
-              return done(new Error('XSRF-TOKEN not set!'));
-            }
-            csrfToken = csrf;
-
-            let sId = extractCookie(res, sessionIdCookieRegex);
-            if (sId === false) {
-              return done();
-            } else {
-              return done(new Error('PHPSESSID is reset! Bad session behaviour'));
-            }
+            return done(error);
           }
+          // console.log('/api/v2/testSession with session token cookies ',res.headers['set-cookie']);
+
+          res.body.ip.should.exist;
+          res.body.entryPoint.should.be.equal('/api/v2/ping');
+          res.body.userAgent.should.match(/^node-superagent/);
+
+          let csrf = extractCookie(res, csrfTokenCookieRegex);
+          if (csrf === false) {
+            return done(new Error('XSRF-TOKEN not set!'));
+          }
+          csrfToken = csrf;
+
+          let sId = extractCookie(res, sessionIdCookieRegex);
+          if (sId === false) {
+            return done();
+          }
+          return done(new Error('PHPSESSID is reset! Bad session behaviour'));
         });
     });
+
     it('sets proper data for /api/v2/testSession WITHOUT session token provided', function (done) {
       supertest(app)
         .get('/api/v2/testSession')
@@ -101,34 +99,32 @@ describe('web application', function () {
         .expect(200)
         .end(function (error, res) {
           if (error) {
-            done(error);
-          } else {
-            // console.log('/api/v2/testSession 2 without session token cookies ',res.headers['set-cookie']);
-
-            res.body.ip.should.exist;
-            res.body.entryPoint.should.be.equal('/api/v2/testSession');
-            res.body.userAgent.should.match(/^node-superagent/);
-
-            let csrf = extractCookie(res, csrfTokenCookieRegex);
-            if (csrf === false) {
-              return done(new Error('XSRF-TOKEN not set!'));
-            }
-            csrfToken = csrf;
-
-            let sId = extractCookie(res, sessionIdCookieRegex);
-
-            if (sId === false) {
-              done(new Error('PHPSESSID not set!'));
-            } else {
-              sessionId = sId;
-              done();
-            }
+            return done(error);
           }
+          // console.log('/api/v2/testSession 2 without session token cookies ',res.headers['set-cookie']);
+
+          res.body.ip.should.exist;
+          res.body.entryPoint.should.be.equal('/api/v2/testSession');
+          res.body.userAgent.should.match(/^node-superagent/);
+
+          let csrf = extractCookie(res, csrfTokenCookieRegex);
+          if (csrf === false) {
+            return done(new Error('XSRF-TOKEN not set!'));
+          }
+          csrfToken = csrf;
+
+          let sId = extractCookie(res, sessionIdCookieRegex);
+
+          if (sId === false) {
+            return done(new Error('PHPSESSID not set!'));
+          }
+          sessionId = sId;
+          done();
         });
     });
   });
 
-  it('it has 200 and pong on /api/v2/state/:state', function (done) {
+  it('has 200 and pong on /api/v2/state/:state', function (done) {
     supertest(app)
       .get('/api/v2/state/00544')
       .set('Cookie', [util.format('PHPSESSID=%s', sessionId)])
@@ -221,7 +217,7 @@ describe('web application', function () {
   });
 
 
-  it('it has 200 on POST /api/v2/update-contact', function (done) {
+  it('has 200 on POST /api/v2/update-contact', function (done) {
     supertest(app)
       .post('/api/v2/update-contact')
       .set('Cookie', [util.format('PHPSESSID=%s', sessionId)])
@@ -233,6 +229,56 @@ describe('web application', function () {
         _csrf: csrfToken
       })
       .expect(200, function (error, res) {
+        if (error) {
+          return done(error);
+        }
+        let csrf = extractCookie(res, csrfTokenCookieRegex);
+        if (csrf === false) {
+          return done(new Error('XSRF-TOKEN not set!'));
+        }
+        csrfToken = csrf;
+        done();
+      });
+  });
+
+  it('has 403 on POST /api/v2/update-contact with missing CSRF token', function (done) {
+    supertest(app)
+      .post('/api/v2/update-contact')
+      .set('Cookie', [util.format('PHPSESSID=%s', sessionId)])
+      .send({
+        firstName: 'test_FirstName_updated',
+        lastName: 'test_LastName_updated',
+        emailAddress: 'test@email.com',
+        phoneNumber: '111-222-3333'
+        // _csrf: csrfToken
+      })
+      .expect(403,'Invalid API Key')
+      .end(function (error, res) {
+        if (error) {
+          return done(error);
+        }
+        let csrf = extractCookie(res, csrfTokenCookieRegex);
+        if (csrf === false) {
+          return done(new Error('XSRF-TOKEN not set!'));
+        }
+        csrfToken = csrf;
+        done();
+      });
+  });
+
+  it('has 403 on POST /api/v2/update-contact with bad CSRF token', function (done) {
+    supertest(app)
+      .post('/api/v2/update-contact')
+      .set('Cookie', [util.format('PHPSESSID=%s', sessionId)])
+      .send({
+        firstName: 'test_FirstName_updated',
+        lastName: 'test_LastName_updated',
+        emailAddress: 'test@email.com',
+        phoneNumber: '111-222-3333',
+        _csrf: 'Во имя Отца, и Сына, и Святаго духа, аминь!'
+      })
+      .expect(403,'Invalid API Key')
+      .end(function (error, res) {
         if (error) {
           return done(error);
         }
