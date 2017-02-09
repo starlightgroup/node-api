24c283e167d1e5f80a05bc89a7d69b98acd3ce97
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-02-09 23:36:12 +0300

    Do not start http server while performing unit tests - it will allow to run tests in parallel

--- server.js
+++ server.js
@@ -182,13 +182,15 @@ app.use(function (err, req, res, next) {
 // console.log("HTTPS Server Started at port : " + https_port);
 
 
-http
-  .createServer(app)
-  .listen(config.PORT, config.HOST, function (error) {
-    if(error){
-      throw error;
-    }
-    console.log("HTTP Server Started at %s:%s", config.HOST, config.PORT );
-  });
+if(!module.parent) {
+  http
+    .createServer(app)
+    .listen(config.PORT, config.HOST, function (error) {
+      if(error){
+        throw error;
+      }
+      console.log("HTTP Server Started at %s:%s", config.HOST, config.PORT);
+    });
+}
 
 module.exports = exports = app;
