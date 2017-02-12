2180a47d9bd98d108154281377cb6129187b1ead
Evan Rusin <evan.rusin@outlook.com>
2017-02-12 07:50:54 +0200

    credit card space removal

--- api/controllers/konnektive.js
+++ api/controllers/konnektive.js
@@ -16,7 +16,7 @@ async function addKonnektiveOrder(req, res, next) {
     body.address2 = xss(req.body.address2);
     body.campaignId = xss(req.body.campaignId);
     body.cardMonth = xss(req.body.cardMonth);
-    body.cardNumber = xss(req.body.cardNumber).replace(" ","");
+    body.cardNumber = xss(req.body.cardNumber).replace(/\s/g,"");
     body.cardYear = xss(req.body.cardYear);
     body.city = xss(req.body.city);
     body.emailAddress = xss(req.body.emailAddress);
@@ -52,7 +52,8 @@ async function addKonnektiveOrder(req, res, next) {
     //body.lastName = req.body.lastName || 'NA';
     //req.body.cardExpiryDate = `${req.body.month}/${req.body.year}`;
     //delete req.body.productId;
-        
+    console.response("--------New Order---------")
+    console.response(body)
 
     const options = {
         uri: 'https://api.konnektive.com/order/import/',
