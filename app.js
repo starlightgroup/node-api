264498a834f2b57099aa64f2670708de182a5d06
Evan Rusin <evan.rusin@outlook.com>
2017-02-16 11:46:28 +0200

    fixed babel-node exit process bug

---
+++ app.js
@@ -0,0 +1,11 @@
+require("babel-register");
+var app = require("./server.js");
+app.listen(8000, function() {
+
+});
+process.title="myserver";
+process.on('SIGINT', function() {
+	process.exit();
+});
+
+module.exports = exports = app;
