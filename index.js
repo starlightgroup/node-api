6b076791edd47efd3537fa8b933b7d61fdb71e4e
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-02-16 14:54:33 +0300

    removed unused check for module parent in executable

--- index.js
+++ index.js
@@ -7,20 +7,19 @@ const
   config = require('./server-config.js'),
   app = require('./app.js');
 
-process.title='myserver';
-process.on('SIGINT', function() {
+process.title = 'myserver';
+process.on('SIGINT', function () {
   process.exit();
 });
 
-if(!module.parent) {
-  http
-    .createServer(app)
-    .listen(config.PORT, config.HOST, function (error) {
-      if(error){
-        throw error;
-      }
-      console.log('HTTP Server Started at %s:%s', config.HOST, config.PORT);
-    });
-}
+http
+  .createServer(app)
+  .listen(config.PORT, config.HOST, function (error) {
+    if (error) {
+      throw error;
+    }
+    console.log('HTTP Server Started at %s:%s', config.HOST, config.PORT);
+  });
+
 
 
