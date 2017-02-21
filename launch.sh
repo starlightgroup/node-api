6610b73f79b3c6fe95edd5db40a696c97fa49798
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-02-22 00:27:38 +0300

    npm insalls forever + small fixes in launch.sh script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
+# script used by deploy CI and CD systems made by @Evan
+
 npm install
 cp -rf /home/flashlightsforever/source/tacticalsales/ public/
-forever stopall
-forever start app.js
+
+./node_modules/.bin/forever stopall
+./node_modules/.bin/forever start app.js
