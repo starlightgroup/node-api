e5457e215b70a91b176d26ec5134ab8933b2c3c8
Evan Rusin <evan.rusin@outlook.com>
2017-02-22 22:28:32 +0200

    launch script fix

--- launch.sh
+++ launch.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # script used by deploy CI and CD systems made by @Evan
-
-npm install
-cp -rf /home/flashlightsforever/source/tacticalsales/ public/
-
 ./node_modules/.bin/forever stopall
+npm install
+npm build
+cp -rf /home/flashlightsforever/source/tacticalsales/ ~/source/public/
 ./node_modules/.bin/forever start index.js
